b5575cb39d9762f8e4356a68ee4c23314e6d3a02
Arshad Mehmood <arshad.mehmood@intel.com>
2024-12-23 21:29:58 +0800

    Added several enhancements
    * Better handling of line with comments
    * Support for .ko path retrieval using mode info
    * Handling of multiple symbols for same name.
    
    Signed-off-by: Arshad Mehmood <arshad.mehmood@intel.com>

--- set_probes_csv.sh
+++ set_probes_csv.sh
@@ -17,15 +17,25 @@
 
 # This bash script reads probe requests from probe.csv and sets up entry and exit probe for request function
 # Caution: Script deletes all previous probes before proceeding. 
-# probe.csv fields: '.so name', 'symbol filter', 'probe name'   (without quotes)
+# probe.csv fields: '.so name', 'process name', 'symbol filter', 'probe name'   (without quotes)
 # Sample csv format:
-#   libgazebo_ros_init.so, GazeboRosInitPrivate::Publish, ros_init_pubtime
+#   libgazebo_ros_init.so, ,GazeboRosInitPrivate::Publish, ros_init_pubtime
 #   <path>/libopenvino_intel_gpu_plugin.so, ,ov::intel_gpu::SyncInferRequest::infer\(\)\s*$,infer_request
-#   <path>/libopenvino_intel_gpu_plugin.so, ,\bcldnn::network::execute_impl\(.*\)$,execute_impl
+#   <path>/libopenvino_intel_gpu_plugin.so, ,\bcldnn::network::execute_impl\(.*\)\s$,execute_impl
 #   <path>/i915.ko, ,\bi915_gem_do_execbuffer,i915_gem_do_execbuffer
 #
-#    ./set_probes_csv.sh probes.csv
+# Grep regex format for symbol filter:
+#   e.g \bcldnn::network::execute_impl\(.*\)$
+#       This regular expression matches lines that begin with the word boundary of the function
+#        cldnn::network::execute_impl() called with any arguments, ensuring it's a separate word,
+#        followed by optional whitespace and ending precisely at the line's end.
 #
+# $ ./set_probes_csv.sh probes.csv
+#
+# If the .so names are not absolute paths, the process name must include the process that utilizes
+# these .so files, and /proc/pid/maps is used to determine their absolute paths. Therefore, the
+# process must be active during the execution of the set_probes_csv.sh script. This requirement
+# is unnecessary when all .so names are provided as absolute paths.
 
 probe_file=$1
 
@@ -39,28 +49,33 @@ fi
 echo "Deleting existing probes"
 sudo perf probe -d '*' 
 
-
-probe_file=$1
-
 while IFS=, read -r library_name process_name symbol_filter probe_name
 do
     # Skip empty lines
     if [ -z $library_name ]; then
         continue
     fi
-        
+
     # Skip comments row
-    if [ "$library_name" == '#' ]; then
+    if [[ $library_name =~ ^# ]]; then
         continue
     fi
 
     # Direct probe call for kernel modules.  no symbol search supported
     if [[ "$library_name" == *.ko ]]; then
-        echo "perf probe -m $library_name -a ${probe_name}_entry=$probe_name"
-        sudo perf probe -m $library_name -a ${probe_name}_entry=$probe_name
+
+        # Use modinfo to get the full path of the kernel module
+        full_path=$(modinfo -n -m "$library_name")
+        if [ -z "$full_path" ]; then
+            echo "Error: Could not find full path for kernel module $library_name"
+            continue
+        fi
+
+        echo "perf probe -m $full_path -a ${probe_name}_entry=$probe_name"
+        sudo perf probe -m $full_path -a ${probe_name}_entry=$probe_name
 
         # Set exit/return probe
-        sudo perf probe -m $library_name -a ${probe_name}=$probe_name%return
+        sudo perf probe -m $full_path -a ${probe_name}=$probe_name%return
         continue
     fi
 
@@ -76,7 +91,7 @@ do
                 fi
                 echo "PID $pid will be used to locate library path for $library_name"
             fi
-            
+
             # Find out library path from loaded list
             library_path=$(cat /proc/$pid/maps | grep  $library_name | tr -s ' ' | cut -d ' ' -f 6 | sort | uniq)
 
@@ -89,6 +104,12 @@ do
     # NOTE: should change -T to -t for .so compiled with debug symbols on
     addresses=$(objdump -t $library_path | c++filt | grep -E $symbol_filter | cut -d ' ' -f 1)
 
+    # If no addresses found, try with -T
+    if [ -z "$addresses" ]; then
+        echo "Trying with -T for better visibility on $library_path and symbol $symbol_filter"
+        addresses=$(objdump -T $library_path | c++filt | grep -E $symbol_filter | cut -d ' ' -f 1)
+    fi
+
     if [ -z "$addresses" ]; then
         echo "Address not found for lib $library_path and symbol $symbol_filter "
         continue
@@ -96,7 +117,10 @@ do
 
     echo "Setting probes on [$library_path] at symbol [$symbol_filter]"
 
-    for address in $addresses
+    addresses_array=($addresses)
+    num_addresses=${#addresses_array[@]}
+
+    for address in "${addresses_array[@]}"
     do
     	address=0x$address
         if [ $((address)) -eq 0 ] 2>/dev/null; then
@@ -104,10 +128,21 @@ do
             continue
         fi
 
+        stripped_address=$(printf "%x" "$address")
+        # Determine if address should be appended due multiple entries for same symbol
+        if [ $num_addresses -gt 1 ]; then
+            entry_name="${probe_name}_0x${stripped_address}_entry"
+            exit_name="${probe_name}_0x${stripped_address}"
+        else
+            entry_name="${probe_name}_entry"
+            exit_name="${probe_name}"
+        fi
+
         # Set entry probe
-        sudo perf probe -x $library_path -f -a ${probe_name}_entry=$address
+        sudo perf probe -x $library_path -f -a "$entry_name=$address"
 
         # Set exit/return probe
-        sudo perf probe -x $library_path -f -a ${probe_name}=$address%return
+        sudo perf probe -x $library_path -f -a "$exit_name=$address%return"
+
     done
 done < "$probe_file"
